28979d8d541d7e9a29b21fc9cb77add18d10c990
Sol Turipe <solcis.bio@gmail.com>
2022-11-13 16:32:53 +0100

    add user implementation

--- src/users/AddUser.js
+++ src/users/AddUser.js
@@ -1,61 +1,94 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function AddUser() {
+  let navigate = useNavigate();
+
+  const [user, setUser] = useState({
+    name: "",
+    lastName: "",
+    email: "",
+    phoneNumber: "",
+  });
+
+  const { name, lastName, email, phoneNumber } = user;
+
+  const onInputChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await axios.post("http://localhost:8080/user", user);
+    navigate("/");
+  };
+
   return (
     <div className="container">
       <div className="row">
         <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
           <h2 className="text-center m-4">Add a Contact</h2>
-          <div className="mb-3">
-            <label htmlFor="name" className="form-label">
-              Name
-            </label>
-            <input
-              type={"text"}
-              className="form-control"
-              placeholder="Enter your name"
-              name="name"
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="lastName" className="form-label">
-              Last Name
-            </label>
-            <input
-              type={"text"}
-              className="form-control"
-              placeholder="Enter your last name"
-              name="lastName"
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="email" className="form-label">
-              E-mail
-            </label>
-            <input
-              type={"text"}
-              className="form-control"
-              placeholder="Enter your email"
-              name="email"
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="phoneNumber" className="form-label">
-              Phone number
-            </label>
-            <input
-              type={"text"}
-              className="form-control"
-              placeholder="Enter your phone number"
-              name="phoneNumber"
-            />
-          </div>
-          <button type="submit" className="btn btn-outline-success">
-            Submit
-          </button>
-          <button type="submit" className="btn btn-outline-danger mx-3">
-            Cancel
-          </button>
+          <form onSubmit={(e) => onSubmit(e)}>
+            <div className="mb-3">
+              <label htmlFor="name" className="form-label">
+                Name
+              </label>
+              <input
+                type={"text"}
+                className="form-control"
+                placeholder="Enter your name"
+                name="name"
+                value={name}
+                onChange={(e) => onInputChange(e)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="lastName" className="form-label">
+                Last Name
+              </label>
+              <input
+                type={"text"}
+                className="form-control"
+                placeholder="Enter your last name"
+                name="lastName"
+                value={lastName}
+                onChange={(e) => onInputChange(e)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">
+                E-mail
+              </label>
+              <input
+                type={"text"}
+                className="form-control"
+                placeholder="Enter your email"
+                name="email"
+                value={email}
+                onChange={(e) => onInputChange(e)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="phoneNumber" className="form-label">
+                Phone number
+              </label>
+              <input
+                type={"text"}
+                className="form-control"
+                placeholder="Enter your phone number"
+                name="phoneNumber"
+                value={phoneNumber}
+                onChange={(e) => onInputChange(e)}
+              />
+            </div>
+            <button type="submit" className="btn btn-outline-success">
+              Submit
+            </button>
+            <button type="submit" className="btn btn-outline-danger mx-3">
+              Cancel
+            </button>
+          </form>
         </div>
       </div>
     </div>
